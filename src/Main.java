7ace2b5403dc722c740d14df342d23964faaaeee
bigliuliu <1552000425@qq.com>
2024-02-26 21:43:01 +0800

    add:完成map作业

--- src/Main.java
+++ src/Main.java
@@ -43,36 +43,91 @@ public class Main {
 //        List<Integer> list3 = List.of(array2);
 //        System.out.println(list3);
 //        ------------使用Map ----------
-        Student s = new Student("ll",56);
-        Map<String,Student> map = new HashMap<>();
-//        将ll 和Student 实例映射并关联
-        map.put("ll",s);
-        Student target = map.get("ll");
+//        Student s = new Student("ll",56);
+//        Map<String,Student> map = new HashMap<>();
+////        将ll 和Student 实例映射并关联
+//        map.put("ll",s);
+//        Student target = map.get("ll");
 //        System.out.println(target.name);
 //        System.out.println(target.score);
 //        System.out.println(map.get("oo"));
-        Map<String, Integer> map1 = new HashMap<>();
-        map1.put("apple1", 123);
-        map1.put("pear", 456);
-        map1.put("orang",12);
-        map1.put("apple2", 789);
+//        Map<String, Integer> map1 = new HashMap<>();
+//        map1.put("apple1", 123);
+//        map1.put("pear", 456);
+//        map1.put("orang",12);
+//        map1.put("apple2", 789);
 //        通过遍历key来遍历map
-        for (String key : map1.keySet()){
-            Integer value = map1.get(key);
-            System.out.println(key+"="+value);
-        }
+//        for (String key : map1.keySet()){
+//            Integer value = map1.get(key);
+//            System.out.println(key+"="+value);
+//        }
 //        同时遍历key和value遍历map
-        for (Map.Entry<String,Integer> entry:map1.entrySet()){
-            String key = entry.getKey();
-            Integer value = entry.getValue();
-            System.out.println(key+"="+value);
-        }
+//        for (Map.Entry<String,Integer> entry:map1.entrySet()){
+//            String key = entry.getKey();
+//            Integer value = entry.getValue();
+//            System.out.println(key+"="+value);
+//        }
+        //        ------------Map练习题 ----------
+        List<Student> list = List.of(
+                new Student("Bob", 78),
+                new Student("Alice", 85),
+                new Student("Brush", 66),
+                new Student("Newton", 99));
+        var holder = new Students(list);
+        System.out.println(holder.getScore("Bob") == 78 ? "测试成功!" : "测试失败!");
+        System.out.println(holder.getScore("Alice") == 85 ? "测试成功!" : "测试失败!");
+        System.out.println(holder.getScore("Tom") == -1 ? "测试成功!" : "测试失败!");
     }
 }
-class Student{
-    public String name;
-    public int score;
-    public Student(String name,int score){
+//class Student{
+//    public String name;
+//    public int score;
+//    public Student(String name,int score){
+//        this.name = name;
+//        this.score = score;
+//    }
+//}
+class Students {
+    List<Student> list;
+    Map<String, Integer> cache;
+
+    Students(List<Student> list) {
+        this.list = list;
+        cache = new HashMap<>();
+    }
+
+    /**
+     * 根据name查找score，找到返回score，未找到返回-1
+     */
+    int getScore(String name) {
+        for(Student ss :list){
+//            将初始化的student类的实例list遍历分别加入map中
+            this.cache.put(ss.name,ss.score);
+        }
+        // 先在Map中查找:
+        Integer score = this.cache.get(name);
+        if (score == null) {
+            // TODO:
+            return  -1;
+        }
+        return  score == null ? -1 : score.intValue();
+    }
+
+    Integer findInList(String name) {
+        for (var ss : this.list) {
+            if (ss.name.equals(name)) {
+                return ss.score;
+            }
+        }
+        return null;
+    }
+}
+
+class Student {
+    String name;
+    int score;
+
+    Student(String name, int score) {
         this.name = name;
         this.score = score;
     }
